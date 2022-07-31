77564ed8de94ce5c1607e82e0a5ef49f100d258c
Joshua-stacks <vasquez.joshua1115@gmail.com>
2022-07-31 19:12:46 -0400

    first merge FE

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,37 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import styled from "styled-components";
+import GlobalStyles from "../GlobalStyles";
+import Footer from "./Footer";
+import Cart from "./Cart";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Header />
+        <Div>
+          <Routes>
+            <Route exact path="/" element={"app"} />
+            <Route exact path="/cart" element={<Cart />} />
+            <Route exact path="/items/:item" element={"item"} />
+          </Routes>
+        </Div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+};
+const Div = styled.div`
+  display: flex;
+  border-right: solid 1px lightgray;
+  border-left: solid 1px lightgray;
 
-function App() {
-  const [bacon, setBacon] = useState(null);
+  margin: auto;
+  width: 70%;
 
-  useEffect(() => {
-    fetch('/bacon')
-      .then(res => res.json())
-      .then(data => setBacon(data));
-  }, []);
-
-  return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
-}
+  height: 87vh;
+`;
 
 export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <Bar>
+      <Web href="https://www.facebook.com/Kijiji.ca/">
+        <FaFacebook />
+      </Web>
+      <Web href="https://twitter.com/kijiji?lang=en">
+        <FaTwitter />
+      </Web>
+      <Web href="https://www.instagram.com/kijijicanada/?hl=en">
+        <FaInstagram />
+      </Web>
+    </Bar>
+  );
+};
+
+const Bar = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: rgb(139, 129, 149);
+  height: 7.2vh;
+  font-size: 40px;
+  color: white;
+`;
+const Web = styled.a`
+  color: white;
+`;
+export default Footer;
